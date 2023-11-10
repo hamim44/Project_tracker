daecda005ba76196daff71a483cb6a7fd3186f1a
Chris Ching <chris@codewithchris.com>
2023-11-10 00:33:02 -0500

    Initial Commit

---
+++ db-project-tracker/db_project_trackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  db_project_trackerApp.swift
+//  db-project-tracker
+//
+//  Created by Chris Ching on 2023-11-10.
+//
+
+import SwiftUI
+
+@main
+struct db_project_trackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
