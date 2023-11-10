05554c837d6e14e6ff71c0697f6f7a869b935d51
Chris Ching <chris@codewithchris.com>
2023-11-10 00:43:36 -0500

    - set up data models - set up container - rename initial view - rename app entry point

---
+++ db-project-tracker/Models/Project.swift
@@ -0,0 +1,24 @@
+//
+//  Project.swift
+//  db-project-tracker
+//
+//  Created by Chris Ching on 2023-11-10.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+class Project: Identifiable {
+    
+    @Attribute(.unique) var id: String
+    var name = ""
+    var startDate = Date()
+    var focus = ""
+    @Relationship(deleteRule: .cascade, inverse: \ProjectUpdate.project)
+    var update: [ProjectUpdate] = [ProjectUpdate]()
+    
+    init() {
+        id = UUID().uuidString
+    }
+}
