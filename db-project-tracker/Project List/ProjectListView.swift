2f0e9483284113064ab32a6877728599bd62ed8f
Chris Ching <chris@codewithchris.com>
2023-11-28 13:38:50 -0500

    - rename conflicting color assets - add project view - add sheet to project list view - add button functionality in project list view to add a new project - add project query and dynamic project list to project list view

--- db-project-tracker/Project List/ProjectListView.swift
+++ db-project-tracker/Project List/ProjectListView.swift
@@ -6,8 +6,14 @@
 //
 
 import SwiftUI
+import SwiftData
 
 struct ProjectListView: View {
+    
+    @State private var newProject: Project?
+    @Query private var projects: [Project]
+    
+    
     var body: some View {
         
         ZStack {
@@ -25,11 +31,9 @@ struct ProjectListView: View {
                 ScrollView (showsIndicators: false) {
                     VStack(alignment: .leading, spacing: 26) {
                         
-                        ProjectCardView()
-                        ProjectCardView()
-                        ProjectCardView()
-                        ProjectCardView()
-                        ProjectCardView()
+                        ForEach(projects) { p in
+                            ProjectCardView(project: p)
+                        }
                     }
                 }
                 
@@ -41,7 +45,8 @@ struct ProjectListView: View {
                 Spacer()
                 HStack {
                     Button(action: {
-                        // Todo
+                        // Create new project
+                        self.newProject = Project()
                     }, label: {
                         ZStack {
                             Circle()
@@ -54,6 +59,10 @@ struct ProjectListView: View {
                 }
             }
             .padding(.leading)
+        }
+        .sheet(item: $newProject) { project in
+            AddProjectView(project: project)
+                .presentationDetents([.fraction(0.2)])
         }
     }
 }
