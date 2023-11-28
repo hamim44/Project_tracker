addced2912e5b94805fda593325d3b980c7385b1
Chris Ching <chris@codewithchris.com>
2023-11-28 00:09:43 -0500

    - add cross image asset - configure colors - configure fonts in FontHelper

---
+++ db-project-tracker/Helpers/FontHelper.swift
@@ -0,0 +1,20 @@
+//
+//  FontHelper.swift
+//  db-project-tracker
+//
+//  Created by Chris Ching on 2023-11-27.
+//
+
+import Foundation
+import SwiftUI
+
+extension Font {
+    
+    static var screenHeading = Font.system(size: 36, weight: .bold)
+    static var bigHeadline = Font.system(size: 15, weight: .bold)
+    static var smallHeadline = Font.system(size: 10, weight: .bold)
+    static var featuredNumber = Font.system(size: 18)
+    static var featuredText = Font.system(size: 12)
+    static var regularText = Font.system(size: 10)
+    static var captionText = Font.system(size: 8)
+}
